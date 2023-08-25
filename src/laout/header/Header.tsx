6595970cf51d29c5db9d147c675cd9952c05a534
User You <useryou@Users-MacBook-Pro.local>
2023-08-25 15:17:07 +0300

    add

--- src/laout/header/Header.tsx
+++ src/laout/header/Header.tsx
@@ -1,25 +1,21 @@
-import styled from "styled-components"
-function Header (){
-    return(
-        <nav>
-            {/* <h1 className="name primary-colour">Jayjay Dinero</h1> */}
-            <StyleMenu className="main">
-            <h1 className="name primary-colour">Jayjay Dinero</h1>
-            <h2 className="home primary-colour color-white">Home</h2>
-            <h2 className="about color-white">About me</h2>
-            <h2 className="projects color-white">Projects</h2>
-            <h2 className="contact color-white">Contact</h2>
-            </StyleMenu>
-          
-           
-        </nav>
-    )
+import styled from "styled-components";
+import Menu from "./menu/Menu";
+function Header() {
+  return (
+    <StyleMenu className="main">
+      {/* <Logo>Jayjay Dinero</Logo> */}
+      <Menu />
+    </StyleMenu>
+  );
 }
 const StyleMenu = styled.div`
-display: inline-flex;
-padding: 19px 160px;
-align-items: flex-start;
-gap: 300px;
-background: #181824;
-`
-export default Header
+  background-color: white;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+// const Logo = styled.span`
+// display: flex;
+
+// `
+export default Header;
---
+++ src/laout/header/menu/Menu.tsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+function Menu() {
+  return (
+    <StyleMenu>
+      <Style>
+        <li>
+          <a href="/name">Jayjay Dinero</a>
+        </li>
+        <li>
+          <a href="/home ">Home</a>
+        </li>
+        <li>
+          <a href="/about">About me</a>
+        </li>
+        <li>
+          <a href="/project">Project</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </Style>
+    </StyleMenu>
+  );
+}
+const StyleMenu = styled.nav`
+  display: flex;
+  gap: 30px;
+`;
+const Style = styled.ul`
+display: flex;
+gap: 30px;
+
+`
+export default Menu;
