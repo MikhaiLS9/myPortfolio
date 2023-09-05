f8471b8eaec1bd1df2069da12f2e4f08d95d0aee
MikhaiLS9 <famm93@mail.ru>
2023-09-05 16:24:49 +0300

    add

--- src/laout/myprojects/Projects.tsx
+++ src/laout/myprojects/Projects.tsx
@@ -2,7 +2,6 @@ import Work from "./work/Work";
 import stream from "../components/photo/xz.jpg";
 import map from "../components/photo/map.jpg";
 import car from "../components/photo/car.jpg";
-import FlexWrapper from "../components/main/FlexWrapper";
 import styled from "styled-components";
 
 function Projects() {
@@ -13,15 +12,19 @@ function Projects() {
         I have worked on many projects over the course of being a Web Developer,
         here are a few of my live, real-world projects
       </TextStyle>
-      <FlexWrapper justify="center">
+      <StyleProject>
         <Work  title="TWINDER" src={map} text="A live Geolocation app for finding tweets and twitter users around you."></Work>
         <Work src={stream} title={"LIVENTS"} text="A video streaming app with live Geolocation, for streaming events."></Work>
         <Work src={car} title={"MOOVE"} text="Mobile app for booking instant pickup & dropoff accross major cities."></Work>
-      </FlexWrapper>
+      </StyleProject>
     </div>
   );
 }
-
+const StyleProject = styled.div`
+gap: 10px;
+display: flex;
+justify-content: center;
+`
 const TitleStyle = styled.h2``;
 const TextStyle = styled.p``;
 export default Projects;
